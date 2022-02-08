007101dc1ef7c9392042de8f7ea643e66cf574a7
Faber Sanchez <faber.reach@gmail.com>
2022-02-07 19:29:22 -0500

    Add Block

---
+++ Src/Blockchain/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Blockchain
+{
+    public interface IBlock
+    {
+        byte[] Data { get; }
+        byte[] Hash { get; set; }
+        int Nonce { get; set; }
+        byte[] PrevHash { get; set; }
+        DateTime TimeStamp { get; }
+    }
+
+
+    public class Block : IBlock
+    {
+        public byte[] Data { get; }
+
+        public byte[] Hash { get; set; }
+        public int Nonce { get; set; }
+        public byte[] PrevHash { get; set; }
+
+        public DateTime TimeStamp { get; }
+
+        public override string ToString()
+        {
+            return $"{Data} " + TimeStamp.ToLongDateString();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
